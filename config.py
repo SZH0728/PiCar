8949b8e644021419ae3bd0c8b5a191c284517c78
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2025-10-11 16:48:17 +0800

    feat(config): 更新配置类结构并调整摄像头参数
    - 引入 BaseConfig 作为基础配置类
    - 将摄像头配置中的 rotation 参数替换为 reverse 参数
    - 新增 ControlConfig 类用于控制相关配置
    - 新增 ProcessConfig 类用于处理流程配置
    - 在主配置类中添加 control 和 process 配置项
    - 实现 ProcessConfig 的属性访问与设置方法
    - 添加 __slots__优化 ProcessConfig 内存使用

--- config.py
+++ config.py
@@ -4,6 +4,8 @@
 from dataclasses import dataclass, field
 from enum import Enum
 
+from process.base import BaseConfig
+
 
 class BaseCameraConfigType(Enum):
     """
@@ -25,7 +27,7 @@ class CameraConfig(object):
     size: tuple[int, int] = (640, 360)  #: 图像尺寸(宽度,高度)
     format: str = 'YUV420'              #: 图像格式
 
-    rotation: int = 180                 #: 图像旋转角度(0/90/180/270)
+    reverse: bool = True                #: 图像翻转
 
     fix_AE_AWB: bool = True             #: 是否固定自动曝光和白平衡
     fix_wait_time: float = 0.5          #: 自动曝光和白平衡固定前的等待时间(秒)
@@ -43,6 +45,49 @@ class MotorConfig(object):
 
 
 @dataclass
+class ControlConfig(object):
+    used: str = 'example'
+
+    save_debug: bool = True
+    interval: int = 30
+
+
+class ProcessConfig(object):
+    __slots__ = ['_process_config_map']
+
+    def __init__(self, process_config_map: dict[str, BaseConfig]):
+        self._process_config_map = process_config_map
+
+    def __getattr__(self, item):
+        if item in self._process_config_map:
+            return self._process_config_map[item]
+
+        raise AttributeError(f"Process {item} config not found")
+
+    def __getitem__(self, item):
+        if not item in self._process_config_map:
+            raise KeyError(f"Process {item} config not found")
+
+        return self._process_config_map[item]
+
+    def __setattr__(self, key: str, value):
+        if key.startswith('_'):
+            super().__setattr__(key, value)
+            return
+
+        if not key in self._process_config_map:
+            raise AttributeError(f"Process {key} config not found")
+
+        self._process_config_map[key] = value
+
+    def __setitem__(self, key, value):
+        if not key in self._process_config_map:
+            raise KeyError(f"Process {key} config not found")
+
+        self._process_config_map[key] = value
+
+
+@dataclass
 class Config(object):
     """
     @brief 全局配置数据类
@@ -50,6 +95,8 @@ class Config(object):
     """
     camera: CameraConfig = field(default_factory=CameraConfig)  #: 摄像头配置
     motor: MotorConfig = field(default_factory=MotorConfig)     #: 电机配置
+    control: ControlConfig = field(default_factory=ControlConfig)
+    process: ProcessConfig | None = None
 
 if __name__ == '__main__':
     pass
